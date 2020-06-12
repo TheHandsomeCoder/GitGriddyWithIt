45cbb81f6333051bcd825e1a0af9523588236414
Scott O'Malley <scottyomalley@gmail.com>
2020-06-12 21:59:17 +0100

    Add initial project details

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid
+{
+    private int width;
+    private int height;
+    private float cellSize;
+    private int[,] gridArray;
+
+    public Grid(int width, int height, float cellSize)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+
+        gridArray = new int[width, height];
+
+        for (int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                CreateWorldText(null, gridArray[x, y].ToString(), GetWorldPosition(x, y), 20, Color.white, TextAnchor.MiddleCenter, TextAlignment.Center);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+
+            }
+        }
+    }
+
+    private Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * cellSize;
+    }
+
+
+    public TextMesh CreateWorldText(Transform parent, string text, Vector3 localPosition, int fontSize, Color color, TextAnchor textAnchor, TextAlignment textAlignment)
+    {
+        GameObject gameObject = new GameObject("World_Text", typeof(TextMesh));
+        Transform transform = gameObject.transform;
+        transform.SetParent(parent, false);
+        transform.localPosition = localPosition;
+        TextMesh textMesh = gameObject.GetComponent<TextMesh>();
+        textMesh.anchor = textAnchor;
+        textMesh.alignment = textAlignment;
+        textMesh.text = text;
+        textMesh.fontSize = fontSize;
+        textMesh.color = color;
+        return textMesh;
+    }
+}
---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    private void Start(){
+        Grid grid = new Grid(4,2, 10f);
+    }
+}
